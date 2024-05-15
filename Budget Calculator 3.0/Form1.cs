d1457c69cf45e35c2680aba13fff9da4237be8b9
Brandon Paul <brandonpaul885@gmail.com>
2024-05-15 18:45:08 -0500

    Add files via upload

---
+++ Budget Calculator 3.0/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Budget_Calculator_3._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox13.Text = Convert.ToString(Convert.ToInt32(textBox4.Text) + (Convert.ToInt32(textBox5.Text) + (Convert.ToInt32(textBox6.Text) + (Convert.ToInt32(textBox32.Text) + (Convert.ToInt32(textBox34.Text) + (Convert.ToInt32(textBox36.Text) + (Convert.ToInt32(textBox38.Text) + (Convert.ToInt32(textBox40.Text)))))))));
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            textBox5.Text = string.Empty;
+            textBox6.Text = string.Empty; 
+            textBox7.Text = string.Empty;
+            textBox8.Text = string.Empty;
+            textBox9.Text = string.Empty;
+            textBox10.Text = string.Empty;
+            textBox11.Text = string.Empty;
+            textBox12.Text = string.Empty;
+            textBox13.Text = string.Empty;
+            textBox14.Text = string.Empty;
+            textBox15.Text = string.Empty;
+            textBox16.Text = string.Empty;
+            textBox17.Text = string.Empty;
+            textBox18.Text = string.Empty; 
+            textBox19.Text = string.Empty;
+            textBox20.Text = string.Empty;
+            textBox21.Text = string.Empty;
+            textBox22.Text = string.Empty;
+            textBox23.Text = string.Empty;
+            textBox24.Text = string.Empty;
+            textBox25.Text = string.Empty;
+            textBox26.Text = string.Empty;
+            textBox27.Text = string.Empty;
+            textBox28.Text = string.Empty;
+            textBox29.Text = string.Empty;
+            textBox30.Text = string.Empty;
+            textBox31.Text = string.Empty;
+            textBox32.Text = string.Empty;
+            textBox33.Text = string.Empty;
+            textBox34.Text = string.Empty;
+            textBox35.Text = string.Empty;
+            textBox36.Text = string.Empty;
+            textBox37.Text = string.Empty;
+            textBox38.Text = string.Empty;
+            textBox39.Text = string.Empty;
+            textBox40.Text = string.Empty;
+            textBox41.Text = string.Empty;
+            textBox42.Text = string.Empty;
+            textBox43.Text = string.Empty;
+            textBox44.Text = string.Empty;
+            textBox45.Text = string.Empty;
+            textBox46.Text = string.Empty;
+            textBox47.Text = string.Empty;
+
+           
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox15_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox43_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox26.Text = Convert.ToString(Convert.ToInt32(textBox21.Text) + (Convert.ToInt32(textBox22.Text) + (Convert.ToInt32(textBox23.Text) + (Convert.ToInt32(textBox24.Text) + (Convert.ToInt32(textBox25.Text) + (Convert.ToInt32(textBox28.Text) + (Convert.ToInt32(textBox30.Text))))))));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox15.Text = Convert.ToString(Convert.ToInt32(textBox21.Text) + (Convert.ToInt32(textBox22.Text) + (Convert.ToInt32(textBox23.Text) + (Convert.ToInt32(textBox24.Text) + (Convert.ToInt32(textBox25.Text) + (Convert.ToInt32(textBox28.Text) + (Convert.ToInt32(textBox30.Text) + (Convert.ToInt32(textBox4.Text) + (Convert.ToInt32(textBox5.Text) + (Convert.ToInt32(textBox6.Text) + (Convert.ToInt32(textBox32.Text) + (Convert.ToInt32(textBox34.Text) + (Convert.ToInt32(textBox36.Text) + (Convert.ToInt32(textBox38.Text) + (Convert.ToInt32(textBox40.Text) - (Convert.ToInt32(textBox10.Text) + (Convert.ToInt32(textBox11.Text) + (Convert.ToInt32(textBox12.Text) + (Convert.ToInt32(textBox41.Text) + (Convert.ToInt32(textBox42.Text)))))))))))))))))))));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox14.Text = Convert.ToString(Convert.ToInt32(textBox10.Text) + (Convert.ToInt32(textBox11.Text) + (Convert.ToInt32(textBox12.Text) + (Convert.ToInt32(textBox41.Text) + (Convert.ToInt32(textBox42.Text))))));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox47.Text = Convert.ToString(Convert.ToInt32(textBox45.Text) / (Convert.ToInt32(textBox46.Text)));
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = string.Empty;
+            textBox5.Text = string.Empty;
+            textBox6.Text = string.Empty;
+            textBox32.Text = string.Empty;
+            textBox34.Text = string.Empty;
+            textBox36.Text = string.Empty;
+            textBox38.Text = string.Empty;
+            textBox40.Text = string.Empty;
+            textBox13.Text = string.Empty;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox21.Text = string.Empty;
+            textBox22.Text = string.Empty;
+            textBox23.Text = string.Empty;
+            textBox24.Text = string.Empty;
+            textBox25.Text = string.Empty;
+            textBox26.Text = string.Empty;
+            textBox28.Text = string.Empty;
+            textBox30.Text = string.Empty;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox10.Text = string.Empty;
+            textBox11.Text = string.Empty;
+            textBox12.Text = string.Empty;
+            textBox14.Text = string.Empty;
+            textBox41.Text = string.Empty;
+            textBox42.Text = string.Empty;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox45.Text = string.Empty;
+            textBox46.Text = string.Empty;
+            textBox47.Text = string.Empty;
+        }
+    }
+}
